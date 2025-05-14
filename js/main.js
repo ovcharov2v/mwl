bf4562f92d92fbbb1ad1280a8ac27a2673c4b67b
Viktor Ovcharov <ovcharov.2v@gmail.com>
2025-05-14 22:40:43 +0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1010,7 +1010,7 @@ document.addEventListener('DOMContentLoaded', function () {
           start: "top 100px",
           end: "+=".concat(content.offsetHeight),
           scrub: 2,
-          markers: true,
+          markers: false,
           pin: true,
           pinSpacer: false
         }
@@ -1027,7 +1027,7 @@ document.addEventListener('DOMContentLoaded', function () {
         duration: 1
       }, '-=1');
       tl.to(cards[2], {
-        y: "-=188%",
+        y: "-=178%",
         duration: 1
       }, '-=1');
       tl.to('.section-system', {
