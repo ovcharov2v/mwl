a82404e192dea189237a2c7c782e84edc732311e
Viktor Ovcharov <ovcharov.2v@gmail.com>
2025-05-14 23:00:15 +0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1021,7 +1021,7 @@ document.addEventListener('DOMContentLoaded', function () {
         duration: 1
       }, '-=1');
       tl.to(cards[1], {
-        y: "-=94%",
+        y: "-=96%",
         scale: 0.9,
         opacity: .9,
         duration: 1
