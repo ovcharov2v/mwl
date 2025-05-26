a347bf4131521efabd26706a0c77b2c011a460f8
Viktor Ovcharov <ovcharov.2v@gmail.com>
2025-05-26 20:16:23 +0400

    Updates

--- js/main.js
+++ js/main.js
@@ -767,16 +767,19 @@ document.addEventListener('DOMContentLoaded', function () {
         crossFade: true
       }
     });
-    slider.autoplay.stop();
-    ScrollTrigger.create({
-      trigger: ".section-company__nav-slider",
-      start: "top 80%",
-      once: true,
-      onUpdate: function onUpdate() {
-        slider.autoplay.start();
-        section.querySelector(".section-company__nav-el[data-slide=\"0\"]").classList.add('section-company__nav-el--active');
-      }
-    });
+    section.querySelector(".section-company__nav-el[data-slide=\"0\"]").classList.add('section-company__nav-el--active');
+
+    /*slider.autoplay.stop()
+      ScrollTrigger.create({
+        trigger: ".section-company__nav-slider",
+        start: "top 80%",
+        once: true,
+        onUpdate: () => {
+            slider.autoplay.start()
+            section.querySelector(`.section-company__nav-el[data-slide="0"]`).classList.add('section-company__nav-el--active')
+        },
+    });*/
+
     var sliderNavEl = section.querySelector('.section-company__nav-slider');
     var sliderNav = new Swiper(sliderNavEl, {
       slidesPerView: 'auto',
